ea229c127611aaff510fc35e2b68b51dd4cd0fc2
Myron R Schippers Jr <dubtwee@gmail.com>
2021-04-01 00:17:50 -0500

    trying to break the jest test

--- frontend/src/modules/randomNumbers/randomNumber.ts
+++ frontend/src/modules/randomNumbers/randomNumber.ts
@@ -1,5 +1,6 @@
-function randomNumber(min: number, max: number): number {
-  return Math.floor(Math.random() * (1 + max - min) + min);
+function randomNumber(min: number, max: number): string {
+  // return Math.floor(Math.random() * (1 + max - min) + min);
+  return 'Hello World';
 }
 
 export default randomNumber;
